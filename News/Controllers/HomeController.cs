85a91fc576a17f6819fe85cb8af5b1543c8ef680
stu <stu@qq.com>
2016-10-24 18:00:19 +0800

    URL及表单数据传递DEMO

--- News/Controllers/HomeController.cs
+++ News/Controllers/HomeController.cs
@@ -37,16 +37,37 @@ namespace News.Controllers
 /// 
 /// </summary>
 /// <returns></returns>
-        public ActionResult List()
+        public ActionResult List(int Page=10)
         {
             String[] data = new string[] { "习近平会见蒙古人民党主席",
                 "今日推荐HOT歼20战机将以制空迷彩涂装亮相珠海航展",
                 "四川八旬夫妻因贫困隐居山洞54年：如今儿是工程师孙是博士",
                 "美科幻战舰将部署亚太 上将:要对付黑暗势力" };
             ViewBag.Data = data;
+            ViewBag.Page = Page;
             return View();
         }
+        /// <summary>
+        /// 添加新闻
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Add()
+        {
 
+            return View();
+        }
+        /// <summary>
+        /// 保存新闻内容
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public ActionResult Seve(string title, string content)
+        {
+            ViewBag.Title = title;
+            ViewBag.Content = content;
+            return View();
+        }
 
     }
 }
